8283739498f3f2cef315facc0d4cc9c82cf6dc01
gnomesley <gnomesley@gmail.com>
2021-07-25 18:20:13 -0400

    Generate const enum instead of regular enum

--- src/ts/enum.ts
+++ src/ts/enum.ts
@@ -5,7 +5,7 @@ import { throwError } from "../util";
 export function printEnum(enumDescriptor: EnumDescriptorProto, indentLevel: number) {
   const printer = new Printer(indentLevel);
   printer.printEmptyLn();
-  printer.printLn(`export enum ${enumDescriptor.getName()} {`);
+  printer.printLn(`export const enum ${enumDescriptor.getName()} {`);
   enumDescriptor.getValueList().forEach(value => {
     const valueName = value.getName() || throwError("Missing value name");
     printer.printIndentedLn(`${valueName.toUpperCase()} = ${value.getNumber()},`);
